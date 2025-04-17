20e11461b193e95b10ac538b39bc39cae5b1abf8
James Skon <skonjkp@kenyon.edu>
2025-04-17 13:37:14 +0000

    Add activity management

--- client/src/routes/App.jsx
+++ client/src/routes/App.jsx
@@ -5,6 +5,7 @@ import RegisterPage from '../pages/RegisterPage';
 import DashboardPage from '../pages/DashboardPage';
 import NavBar from '../components/NavBar';
 import { UserProvider, useUser } from '../context/UserContext';
+import ManageActivitiesPage from '../pages/ManageActivitiesPage';
 
 function AppRoutes() {
   const { user } = useUser();
@@ -16,6 +17,7 @@ function AppRoutes() {
         <Route path="/" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/dashboard" element={<DashboardPage user={user} />} />
+	<Route path="/manage-activities" element={<ManageActivitiesPage />} />  
       </Routes>
     </>
   );
